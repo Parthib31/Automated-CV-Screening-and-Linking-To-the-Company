ef1c88522c2bbdb892b61f47471f701a53f3687b
Parthib Chowdhury <parthibchowdhury31@gmail.com>
2023-04-29 19:55:16 +0530

    Automation done

--- app.js
+++ app.js
@@ -1,44 +1,58 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const fs=require("fs");
-const multer=require("multer");
-const { exec } = require('child_process');
-const path = require('path');
-const { format } = require('path');
-const session = require('express-session');
-const passport=require("passport");
-const passportLocalMongoose=require("passport-local-mongoose");
+//Module declaration:
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const multer = require("multer");
+const { exec } = require("child_process");
+const path = require("path");
+const { format } = require("path");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const { MIMEType } = require("util");
+const Docx = require("docx");
+const mammoth = require("mammoth");
 
+//File upload directory initialisation:
 const storage = multer.diskStorage({
-  destination: 'uploads/',
+  destination: "uploads/",
   filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now()+path.extname(file.originalname));
-  }
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
 });
 
-const upload = multer({ storage:storage });
+const upload = multer({ storage: storage });
 
+//app declaration:
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.set("view engine","ejs");
-app.use(express.static('public'));
-// app.use(express.static('Images'));
-app.use(express.static('uploads'));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.static("uploads"));
 
-app.use(session({
-  secret: 'thisissecret',
-  resave: false,
-  saveUninitialized: false
-  // cookie: { secure: true }
-}));
+//session initialisation:
+app.use(
+  session({
+    secret: "thisissecret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect('mongodb+srv://parthib:formDB%40123@jobdb.ogpinmf.mongodb.net/formdatabase', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose connection:
+mongoose.connect("mongodb://127.0.0.1:27017/formdatabase", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //userSchema:
 const userSchema = new mongoose.Schema({
@@ -49,19 +63,22 @@ userSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model("User", userSchema);
 
+//passport authentication:
 passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 //Job application Schema:
-const dataSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  phone:Number,
-  coverletter: String,
-  resumeName:String
+const formSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: Number, required: true },
+  resume: String,
+  coverletter: { type: String, required: true },
 });
-const Data = mongoose.model('Data', dataSchema);
+const Form1 = mongoose.model("Web_dev", formSchema);
+const Form2 = mongoose.model("App_dev", formSchema);
+const Form3 = mongoose.model("Data_Analyst", formSchema);
 
 //Router definition:
 app.get("/login", (req, res) => {
@@ -72,89 +89,266 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
-app.get("/logout",function(req,res,next){
-  req.logout(function(err) {
-    if (err) { return next(err); }
-    res.redirect('/');
+app.get("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
   });
-})
+});
 
 app.get("/home", (req, res) => {
-  if(req.isAuthenticated()){
+  if (req.isAuthenticated()) {
     res.render("index");
-  } else{
+  } else {
     res.redirect("/login");
   }
 });
 
-app.get('/', (req, res) => {
-  if(req.isAuthenticated()){
+app.get("/", (req, res) => {
+  if (req.isAuthenticated()) {
     res.redirect("/home");
-  } else{
+  } else {
     res.redirect("/login");
   }
 });
 
-app.get('/form', (req, res) => {
+app.get("/form", (req, res) => {
   res.render("user-form.ejs");
-  // res.render("success.ejs");  
 });
 
 //user details post:
 app.post("/register", async function (req, res) {
-  
-  User.register({username:req.body.username},req.body.password,function(err,user){
-    if(err)
-    {
-      console.error(err);
-      res.redirect("/register");
+  User.register(
+    { username: req.body.username },
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        console.error(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/home");
+        });
+      }
     }
-    else
-    {
-      passport.authenticate("local")(req,res,function(){
-        res.redirect("/home");
-      });
-    }
-  })
+  );
 });
 
 app.post("/login", async (req, res) => {
-  const newUser=new User({
-    username:req.body.username,
-    password:req.body.password
+  const newUser = new User({
+    username: req.body.username,
+    password: req.body.password,
   });
 
-  req.login(newUser,function(err){
-    if(err)
-    {
+  req.login(newUser, function (err) {
+    if (err) {
       console.error(err);
       res.redirect("/login");
-    } else{
-      passport.authenticate("local")(req,res,function(){
+    } else {
+      passport.authenticate("local")(req, res, function () {
         res.redirect("/home");
       });
     }
-  })
+  });
 });
 
 //job application post:
-app.post('/form',upload.single('resume'), async (req, res) => {
-  if(req.file){
-    const data = new Data({
-      name: req.body.name,
-      email: req.body.email,
-      phone:req.body.phone,
-      coverletter:req.body.coverletter,
-      resumeName:req.file.filename
+app.post("/form", upload.single("resume"), async (req, res) => {
+
+  res.render("success");
+
+  const formData = {
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    resume: req.file.filename,
+    coverletter: req.body.coverletter,
+  };
+
+  //Web Dev Company
+  mammoth.extractRawText({ path: "./webDevDataset.docx" }).then(function (result1) {
+    let text1 = result1.value;
+
+    const filePath = path.join("./uploads", req.file.filename);
+    mammoth.extractRawText({ path: filePath }).then(function (result2) {
+      let text2 = result2.value;
+
+      var ans = calculateMatchPercentage(text1, text2); //calculating match percentage
+      console.log(ans);
+
+      if (ans >= 60) {
+        Form1.create(formData).then((item, err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            item.save();
+          }
+        });
+      }
     });
-      await data.save();
-      res.render('success.ejs');   
+  });
+
+  //App Dev Company
+  mammoth.extractRawText({ path: "./appDevDataset.docx" }).then(function (result1) {
+    let text1 = result1.value;
+
+    const filePath = path.join("./uploads", req.file.filename);
+    mammoth.extractRawText({ path: filePath }).then(function (result2) {
+      let text2 = result2.value;
+
+      var ans1 = calculateMatchPercentage(text1, text2); //calculating match percentage
+      console.log(ans1);
+
+      if (ans1 >= 60) {
+        Form2.create(formData).then((item, err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            item.save();
+          }
+        });
+      }
+    });
+  });
+
+  //Data Analyst Company
+  mammoth.extractRawText({ path: "./DataAnalystDataset.docx" }).then(function (result1) {
+    let text1 = result1.value;
+
+    const filePath = path.join("./uploads", req.file.filename);
+    mammoth.extractRawText({ path: filePath }).then(function (result2) {
+      let text2 = result2.value;
+
+      var ans2 = calculateMatchPercentage(text1, text2); //calculating match percentage
+      console.log(ans2);
+
+      if (ans2 >= 60) {
+        Form3.create(formData).then((item, err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            item.save();
+          }
+        });
+      }
+    });
+  });
   
-}else{
-  console.log("error");
-}
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.get("/web", function (req, res) {
+  Form1.find({}).then((data, err) => {
+    if (err) {
+      console.log(err);
+    }
+    res.render("datas", { data: data });
+  });
 });
+
+app.get("/app", function (req, res) {
+  Form2.find({}).then((data, err) => {
+    if (err) {
+      console.log(err);
+    }
+    res.render("datas", { data: data });
+  });
+});
+
+app.get("/data", function (req, res) {
+  Form3.find({}).then((data, err) => {
+    if (err) {
+      console.log(err);
+    }
+    res.render("datas", { data: data });
+  });
+});
+
+app.get("/company/:filename", function (req, res) {
+  const filePath = path.join("./uploads", req.params.filename);
+  const readStream = fs.createReadStream(filePath);
+  res.setHeader(
+    "Content-disposition",
+    "attachment; filename=" + req.params.filename
+  );
+  readStream.pipe(res);
+});
+
+//Sever Declaration:
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
+
+//Function for calculating match percentage:
+function calculateMatchPercentage(text1, text2) {
+  const cleanText1 = text1
+    .replace(/[.,\/#!$%\^&|\*;@:{}=\-_`~()]/g, "")
+    .toLowerCase();
+  const cleanText2 = text2
+    .replace(/[.,\/#!$%\^&|\*;@:{}=\-_`~()]/g, "")
+    .toLowerCase();
+
+  //For resume:
+  let wordArray1 = new Array(new Array());
+  let wordArray2 = new Array(new Array());
+  wordArray2[0] = [];
+
+  var i = 0,
+    j = 0;
+  for (var k = 0; k < cleanText2.length; k++) {
+    j = 0;
+    if (cleanText2[k] !== " " && cleanText2[k] !== "\n") {
+      wordArray2[i].push(cleanText2[k]);
+      j++;
+    } else if (cleanText2[k] === " " && cleanText2[k - 1] === " ") {
+      continue;
+    } else if (cleanText2[k] === "\n" && cleanText2[k - 1] === "\n") {
+      continue;
+    } else {
+      i++;
+      wordArray2[i] = [];
+    }
+  }
+
+  //For dataset
+  var i = 0,
+    j = 0;
+  wordArray1[0] = [];
+  for (var k = 0; k < cleanText1.length; k++) {
+    j = 0;
+    if (cleanText1[k] !== " " && cleanText1[k] !== "\n") {
+      wordArray1[i].push(cleanText1[k]);
+      j++;
+    } else if (cleanText1[k] === " " && cleanText1[k - 1] === " ") {
+      continue;
+    } else if (cleanText1[k] === "\n" && cleanText1[k - 1] === "\n") {
+      continue;
+    } else {
+      i++;
+      wordArray1[i] = [];
+    }
+  }
+
+  //Calculating Matching percentage:
+  let matched_pair = 0;
+
+  for (
+    var i = 0;
+    i < wordArray1.length;
+    i++ //dataset traverse
+  ) {
+    for (
+      var j = 0;
+      j < wordArray2.length;
+      j++ //resume traverse
+    ) {
+      if (JSON.stringify(wordArray1[i]) === JSON.stringify(wordArray2[j])) {
+        matched_pair++;
+        break;
+      }
+    }
+  }
+  var ans = (matched_pair / wordArray1.length) * 100;
+  return ans;
+}
